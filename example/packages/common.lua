1b80c5a7b22b9e0623e3b47a4f51718f6856bd03
Chen Yufei <cyfdecyf@gmail.com>
2022-03-10 18:06:45 +0800

    Specify version for libunwind.

--- example/packages/common.lua
+++ example/packages/common.lua
@@ -1,3 +1,11 @@
 function gflags_configs()
-    return {system = false, configs = {mt = true, shared = true}}
+    return {version = "v2.2.2", system = false, configs = {mt = true, shared = false}}
+end
+
+function glog_configs()
+    return {version = "v0.5.0", system = false, configs = {gflags = true, unwind = true, shared = false}}
+end
+
+function libunwind_configs()
+    return {version = "v1.5", system = false}
 end
--- example/packages/glog.lua
+++ example/packages/glog.lua
@@ -1,3 +1,4 @@
 includes("common.lua")
-add_requires("glog", {system = false, configs = {gflags = true, shared = true}})
+add_requires("glog", glog_configs())
 add_requireconfs("glog.gflags", gflags_configs())
+add_requireconfs("glog.libunwind", libunwind_configs())
